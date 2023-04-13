7ee2a1364472055512a66645afe31ef7c3cb72b9
Mustafa Güler <mustfguler@hotmail.com>
2023-04-13 10:24:36 +0300

    EmojiCopy Created.

--- src/test/EmojiCopy.test.js
+++ src/test/EmojiCopy.test.js
@@ -3,18 +3,18 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "../App";
 
-// Bir emojiye tıklandığında başarıyla kopyalandığını test eder
+// Tests that clicking an emoji successfully copied
 describe("Emoji Copy Test", () => {
-  // her testten önce çalışarak App dosyasını render eder
+  // renders the App file by running before each test
   beforeEach(() => {
     render(<App />);
   });
 
   test("When emoji clicked it must be copied", () => {
-    const emoji = screen.getByText("Wink"); // test için wink isimli emoji verisi çekildi
-    fireEvent.click(emoji); // çekilen emojiye tıklandı
+    const emoji = screen.getByText("Grin"); // Emoji named wink was retrieved for testing
+    fireEvent.click(emoji); // clicked on the drawn emoji
     expect(emoji.parentElement.getAttribute("data-clipboard-text")).toMatch(
-      "😉"
-    ); // kopyalanan emojinin doğruluğu test edildi
+      "😁"
+    ); // the correctness of the copied emoji is tested
   });
 });
