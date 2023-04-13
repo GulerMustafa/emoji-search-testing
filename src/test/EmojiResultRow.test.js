50585a6595e6d0810c06dfa4703a6b13354f759d
Mustafa Güler <mustfguler@hotmail.com>
2023-04-13 10:27:19 +0300

    EmojiResultRow.test Created.

--- src/test/EmojiResultRow.test.js
+++ src/test/EmojiResultRow.test.js
@@ -3,11 +3,11 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "../App";
 
-// Sayfa yüklendiğinde emoji listesinin düzgün görüntülenmesini kontrol eder
+// Controls the correct display of the emoji list when the page is loaded
 describe("Emoji list render process", () => {
   test("Emoji list must be rendered properly", () => {
-    render(<App />); // kontrol edilecek dosya render edilir
-    const listItems = screen.getAllByTestId("row"); // test id bilgisine göre list itemları çekildi
-    expect(listItems.length).toEqual(20); // çekilen list itemlarının sayısının denkliği kontrol edildi
+    render(<App />); // the file to be checked is rendered
+    const listItems = screen.getAllByTestId("row"); // List items are pulled according to the test id information
+    expect(listItems.length).toEqual(20); // check the equivalence of the number of selected list items
   });
 });
